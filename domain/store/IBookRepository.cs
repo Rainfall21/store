6126d178300c085c4253f2a8a7102e2b660faff1
Ismagulov <na.ismagulov@gmail.com>
2021-11-19 08:54:35 +0300

    Create web application

--- domain/store/IBookRepository.cs
+++ domain/store/IBookRepository.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace store
+namespace Store
 {
-    interface IBookRepository
+    public interface IBookRepository
     {
+        Book[] GettAllByTitle(string titlePart);
     }
 }
--- infrastructure/StoreMemory/BookRepository.cs
+++ infrastructure/StoreMemory/BookRepository.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.Linq;
 
-namespace StoreMemory
+namespace Store.Memory
 {
-    public class Class1
+    public class BookRepository : IBookRepository
     {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Shawshank Redemption"),
+            new Book(2, "The Green Mile"),
+            new Book(3, "The Catcher in the Rye"),
+        };
+
+        public Book[] GettAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
     }
 }
