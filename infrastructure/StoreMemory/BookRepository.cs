02bba1659fc879b0cc21d81bef8cb48c6036eaad
Ismagulov <na.ismagulov@gmail.com>
2021-11-22 21:01:32 +0300

    Realization of a serch by ISBN and Author

--- infrastructure/StoreMemory/BookRepository.cs
+++ infrastructure/StoreMemory/BookRepository.cs
@@ -7,14 +7,21 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "Shawshank Redemption"),
-            new Book(2, "The Green Mile"),
-            new Book(3, "Catcher in the Rye"),
+            new Book(1, "ISBN 12345-54321","G. Orwell", "1984"),
+            new Book(2, "ISBN 12345-54322","S. King", "The Green Mile"),
+            new Book(3, "ISBN 12345-54323","J.D. Salinger", "Catcher in the Rye"),
         };
 
-        public Book[] GetAllByTitle(string titlePart)
+        public Book[] GetAllByIsbn(string isbn)
         {
-            return books.Where(book => book.Title.Contains(titlePart))
+            return books.Where(book => book.Isbn == isbn)
+                        .ToArray();
+        }
+
+        public Book[] GetAllByTitleOrAuthor(string query)
+        {
+            return books.Where(book => book.Author.Contains(query)
+                                     ||book.Title.Contains(query))
                         .ToArray();
         }
     }
