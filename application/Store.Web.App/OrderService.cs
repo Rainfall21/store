5277b3e331e889312212b2a91220412bafce4e40
Ismagulov <na.ismagulov@gmail.com>
2022-02-05 18:54:26 +0300

    add the beginning of message sender

--- application/Store.Web.App/OrderService.cs
+++ application/Store.Web.App/OrderService.cs
@@ -197,6 +197,8 @@ namespace Store.Web.App
             orderRepository.Update(order);
             Session.RemoveCart();
 
+            //notificationService.StartProcess(order);
+
             return Map(order);
         }
     }
